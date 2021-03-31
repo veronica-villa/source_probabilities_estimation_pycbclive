fc9d47c756730348cd191f0dd0123e25f9fe41c3
veronica-villa <veronica.villa@ligo.org>
2021-03-31 16:34:06 -0700

    Improved scripts

--- probabilities_from_arguments.py
+++ probabilities_from_arguments.py
@@ -5,88 +5,89 @@ import json
 import shutil
 import argparse
 import math
-import mchirp_area
+from matplotlib import use; use('Agg')
+from matplotlib import pyplot as plt
+from pycbc import mchirp_area
 from pycbc.cosmology import _redshift
+from pycbc.conversions import mass2_from_mchirp_mass1 as m2mcm1
+import mc_area_plots
 
 def z_estimation(dist, dist_std):
-    z_est = _redshift(dist)
+    z_estimation = _redshift(dist)
     z_est_max = _redshift(dist + dist_std)
     z_est_min = _redshift(dist - dist_std)
-    z_std_est = 0.5 * (z_est_max - z_est_min)
-    return z_est, z_std_est
+    z_std_estimation = 0.5 * (z_est_max - z_est_min)
+    z = {'central': z_estimation, 'delta': z_std_estimation}
+    return z
 
 parser = argparse.ArgumentParser()
+parser.add_argument('--superevent', help='Superevent ID.')
+parser.add_argument('--event', help='Event ID.')
+parser.add_argument('--pipeline')
 parser.add_argument('--mchirp', type=float)
-parser.add_argument('--effective-distance', type=float)
-parser.add_argument('--bayestar-distance', type=float)
-parser.add_argument('--bayestar-delta', type=float)
+parser.add_argument('--min-eff-distance', type=float)
 parser.add_argument('--coinc-snr', type=float)
-parser.add_argument('--superevent',
-                    help='Superevent ID.')
-parser.add_argument('--event',
-                    help='PyCBC event ID.')
+parser.add_argument('--bayestar-distance', type=float)
+parser.add_argument('--bayestar-std', type=float)
 parser.add_argument('--no-redshift', action='store_true')
+parser.add_argument('--output-dir', nargs='+')
+parser.add_argument('--enable-mass-plot', action='store_true')
+parser.add_argument('--enable-probability-plot', action='store_true')
 
 mchirp_area.insert_args(parser)
+mc_area_plots.insert_args(parser)
 args = parser.parse_args()
 
-mass_limits = {'max_m1': args.max_m1, 'min_m2': args.min_m2}
-mass_bdary = {'ns_max': args.ns_max, 'gap_max': args.gap_max}
+mc_area_args = mchirp_area.from_cli(args)
+contour_plot_args = mc_area_plots.from_cli(args)
+mass_limits = mc_area_args['mass_limits']
+mass_bdary = mc_area_args['mass_bdary']
+coeff = mc_area_args['estimation_coeff']
+separate_mass_gap = mc_area_args['mass_gap']
 
-mchirp = args.mchirp
-mchirp_delta = mchirp*args.mchirp_to_delta_coeff
-trig_mc = {'central': mchirp, 'delta': mchirp_delta}
+trig_mc_det = {'central': args.mchirp, 'delta': args.mchirp * coeff['m0']}
 
+#estimate redshift
 if args.no_redshift:
-    z = 0.0
-    z_delta = 0.0
+    z = {'central': 0.0, 'delta': 0.0}
+    dir_name = 'p_no_redshift'
 else:
-    if args.effective_distance:
-        lum_dist = args.effective_distance * args.eff_to_lum_distance_coeff
-        b = args.lum_distance_to_delta_coeff
-        lum_dist_std = (lum_dist * math.exp(b[0]) * args.coinc_snr ** b[1])
-    elif args.bayestar_distance:
-        lum_dist = args.bayestar_distance
-        lum_dist_std = args.bayestar_delta
-    z, z_delta = z_estimation(lum_dist, lum_dist_std)
+    if args.min_eff_distance and args.coinc_snr:
+        dist_estimation = coeff['a0'] * args.min_eff_distance
+        dist_std_estimation = (dist_estimation * math.exp(coeff['b0']) *
+                               args.coinc_snr ** coeff['b1'])
+        z = z_estimation(dist_estimation, dist_std_estimation)
+        dir_name = 'p_dist_estimated'
+    elif args.bayestar_distance and args.bayestar_std:
+        z = z_estimation(args.bayestar_distance, args.bayestar_std)
+        dir_name = 'p_dist_bayestar'
 
-z = {'central': z, 'delta': z_delta}
+areas = mchirp_area.calc_areas(trig_mc_det, mass_limits, mass_bdary, z,
+                               separate_mass_gap)
+total_area = sum(areas.values())
+probabilities = {key: areas[key]/total_area for key in areas}
 
-data = {'superevent': args.superevent, 'event': args.event, 'mchirp': mchirp,
-        'min_eff_dist': args.effective_distance, 'coinc_snr': args.coinc_snr}
-
-out_dir = 'probabilities_args/' + args.superevent + '/'
-pub_dir = ('/home/veronica.villa/public_html/source_probabilities/o3/' +
-           args.superevent + '/')
-dirs = [out_dir, pub_dir]
+dirs = [out_dir + dir_name + '/' for out_dir in args.output_dir]
 for dir in dirs:
     if not os.path.exists(dir):
-        print(dir)
         os.makedirs(dir)
 
-name = (str('%s_%s%s%s%s.txt' %
-        (args.superevent, args.event, '_MG-separate' if
-         args.mass_gap_separate is not False else '',
-         '_no-z' if args.no_redshift else ('_dist-estimated' if args.effective_distance else '_bay-dist'))))
-
-out_name = str(out_dir + name)
-pub_name = str(pub_dir + name)
-names = [out_name, pub_name]
-
-prob_args = {'trig_mc': trig_mc, 'mass_limits': mass_limits, 'mass_bdary':
-             mass_bdary, 'z': z, 'mass_gap': args.mass_gap_separate,
-             'mass_plot': args.enable_mass_plot, 'probability_plot':
-             args.enable_probability_plot, 'fnames': names}
-
-probabilities = mchirp_area.calc_probabilities(prob_args)
-
+name = str('%s-%s-%s.json' % (args.superevent, args.event, args.pipeline))
+names = [dir + name for dir in dirs]
 for name in names:
     with open(name, 'w') as outfile:
-        outfile.write('Source probabilities for %s - %s \n' % 
-                      (args.superevent, args.event))
-        outfile.write('\n' + 'data: ')
-        json.dump(data, outfile, indent=1)
-        outfile.write('\n' + 'probabilities: ')
-        json.dump(probabilities, outfile, indent=1)
+        json.dump(probabilities, outfile)
 
-print('Done!')
+if args.enable_mass_plot:
+    massplot = mc_area_plots.contour_mass_plot(trig_mc_det, mass_limits,
+                                                   mass_bdary, z,
+                                                   contour_plot_args)
+    for name in names:
+        plot_name = name.replace('.json', '_mass_plot.png')
+        massplot.savefig(plot_name)
+
+if args.enable_probability_plot:
+    probsplot = mc_area_plots.probabilities_plot(probabilities)
+    for name in names:
+        plot_name = name.replace('.json', '_probabilities_pie.png')
+        probsplot.savefig(plot_name)
--- probabilities_from_gracedb.py
+++ probabilities_from_gracedb.py
@@ -1,199 +1,98 @@
 #!/usr/bin/env python
+
 import os.path
 import json
-import subprocess
-import shutil
 import argparse
-import click
 import math
+from matplotlib import use; use('Agg')
+from matplotlib import pyplot as plt
 from ligo.gracedb.rest import GraceDb
-from ligo.gracedb.rest import HTTPError
-from astropy.io import fits
-import mchirp_area
+from pycbc import mchirp_area
 from pycbc.cosmology import _redshift
-
-def check_superevents(superevents):
-    """Checks if the superevents exist in the GraceDb database. Then, checks
-       if they contain any pycbc trigger. Returns a list with the pycbc events
-       corresponding with the superevents that satisfy both conditions.
-    """
-    for superev in superevents:
-        try:
-            g.superevent(superev)
-        except HTTPError:
-            print('Superevent %s does not exist!' %superev)
-            if click.confirm('Do you want to enter another superevent ID?'):
-                val = click.prompt('Please enter a valid superevent')
-                (args.superevents).append(val)
-                continue
-        else:
-            try_ev = [ev['graceid'] for ev in
-                      g.events('superevent: %s pipeline: pycbc' % superev)]
-            if len(try_ev) == 0:
-                print('No pycbc trigger for superevent %s.' % superev)
-                continue
-            else:
-                e_id.extend(try_ev)
-    return e_id
-
-
-def check_events(events):
-    """Checks if the events exist in the GraceDb database and if they are
-       pycbc triggers. Returns a list with the events that satisfy both
-       conditions.
-    """
-    for event in args.events:
-        try:
-            ev = g.event(event).json()
-        except HTTPError:
-            print('Event %s does not exist!' % event)
-            if click.confirm('Do you want to enter another pycbc event ID?'):
-                val = click.prompt('Please enter a valid pycbc event')
-                (args.events).append(val)
-                continue
-        else:
-            if ev['pipeline'] != 'pycbc':
-                print('%s is not a pycbc event.' % event)
-            else:
-                e_id.append(event)
-    return e_id
-
+import mc_area_plots
 
 def z_estimation(dist, dist_std):
-    z_est = _redshift(dist)
+    z_estimation = _redshift(dist)
     z_est_max = _redshift(dist + dist_std)
     z_est_min = _redshift(dist - dist_std)
-    z_std_est = 0.5 * (z_est_max - z_est_min)
-    return z_est, z_std_est
-
-
-def get_data(event_id, args):
-    """Gets the following properties of the event from GraceDb database:
-       superevent ID, mchirp, coincident snr and effective distance for each
-       ifo, the bayesian mean distance and its std. Then, computes other
-       properties of the event: uncertainty of mchirp, and redshift and its
-       uncertainty.
-    """
-    ev = g.event(event_id).json()
-    s_id = ev['superevent']
-    ifos = [ifos['ifo'] for ifos in ev['extra_attributes']['SingleInspiral']]
-    eff_dist = [ifos['eff_distance'] if 'eff_distance' in ifos else -1
-                for ifos in ev['extra_attributes']['SingleInspiral']]
-    snr = ev['extra_attributes']['CoincInspiral']['snr']
-    mchirp = ev['extra_attributes']['CoincInspiral']['mchirp']
-    mchirp_delta = mchirp*args.mchirp_to_delta_coeff
-
-    if args.estimate_lum_dist is True:
-        dist_estimation = min(eff_dist) * args.eff_to_lum_distance_coeff
-        b = args.lum_distance_to_delta_coeff
-        dist_std_estimation = (dist_estimation * math.exp(b[0]) * snr ** b[1])
-        z, z_delta = z_estimation(dist_estimation, dist_std_estimation)
-        return {'s_id': s_id, 'e_id': event_id, 'mchirp': mchirp, 'mchirp_delta':
-                mchirp_delta, 'snr': snr, 'z': z, 'z_delta': z_delta,
-                'eff_dist': {ifo: eff_dist[n] for n, ifo in enumerate(ifos)},
-                'dist_estimation': dist_estimation,
-                'dist_std_estimation': dist_std_estimation}
-
-    # If it is not True, obtain BAYESTAR distance and std from a .fits file
-    if os.path.exists('fits_files/%s.fits' % event_id):
-        pass
-    else:
-        files = g.files(event_id).json()
-        fits_file = [file for file in files if file.endswith('fits')]
-        if len(fits_file) == 0:
-
-            xml_file = [file for file in files if 'coinc' in file
-                        and file.endswith('xml')][0]
-
-            xml_files = [file for file in files if file.endswith('xml')]
-            for file in xml_files:
-                out = open(file, 'wb')
-                r = g.files(event_id, file)
-                out.write(r.read())
-                out.close()
-
-            outfile = open('xml_files/%s.xml' % event_id,'wb')
-            r = g.files(event_id,xml_file)
-            outfile.write(r.read())
-            outfile.close()
- 
-            file_path = 'xml_files/' + str(event_id) + '.xml'
-            bayestar = ['bayestar-localize-coincs', file_path, file_path]
-            subprocess.call(bayestar)
-            file_name = 'fits_files/' + str(event_id) + '.fits'
-            shutil.move('./0.fits', file_name)
-        elif len(fits_file) == 1:
-            file_name = fits_file[0]
-        else:
-            file_name = [file for file in fits_file if
-                         file.startswith('bayestar')][0]
-        outfile = open('fits_files/%s.fits' % event_id,'wb')
-        r = g.files(event_id,file_name)
-        outfile.write(r.read())
-        outfile.close()
-    hdul = fits.open('fits_files/%s.fits' % event_id)
-    for i in range(len(hdul)):
-        for j in hdul[i].header.keys():
-            if j == 'DISTMEAN':
-                bay_dist = hdul[i].header[j]
-            if j == 'DISTSTD':
-                bay_std = hdul[i].header[j]
-    z = _redshift(bay_dist)
-    dist_max = bay_dist + bay_std
-    dist_min = bay_dist - bay_std
-    z_delta = 0.5 * (_redshift(dist_max) - _redshift(dist_min))
-    return {'s_id': s_id, 'e_id': event_id, 'mchirp': mchirp, 'mchirp_delta':
-            mchirp_delta, 'snr': snr, 'z': z, 'z_delta': z_delta,
-            'eff_dist': {ifo: eff_dist[n] for n,ifo in enumerate(ifos)},
-            'bay_dist': bay_dist, 'bay_std': bay_std}
+    z_std_estimation = 0.5 * (z_est_max - z_est_min)
+    z = {'central': z_estimation, 'delta': z_std_estimation}
+    return z
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--superevents', nargs='+',
-                    help='Superevent IDs.')
-parser.add_argument('--events', nargs='+',
-                    help='PyCBC event IDs.')
-parser.add_argument('--estimate-lum-dist', action='store_true',
-                    help='Probabilities are computed with luminosity '
-                         'distances estimated from effective distances.')
+parser.add_argument('--event', help='GraceDB event ID.')
+parser.add_argument('--no-redshift', action='store_true')
+parser.add_argument('--output-dir', nargs='+')
+parser.add_argument('--enable-mass-plot', action='store_true')
+parser.add_argument('--enable-probability-plot', action='store_true')
 
 mchirp_area.insert_args(parser)
+mc_area_plots.insert_args(parser)
 args = parser.parse_args()
 
-directories = ['xml_files', 'fits_files', 'probabilities']
-for dir in directories:
-    if not os.path.exists(dir):
-        os.mkdir(dir)
+mc_area_args = mchirp_area.from_cli(args)
+contour_plot_args = mc_area_plots.from_cli(args)
+mass_limits = mc_area_args['mass_limits']
+mass_bdary = mc_area_args['mass_bdary']
+coeff = mc_area_args['estimation_coeff']
+separate_mass_gap = mc_area_args['mass_gap']
+
 g = GraceDb()
-mass_limits = {'max_m1': args.max_m1, 'min_m2': args.min_m2}
-mass_bdary = {'ns_max': args.ns_max, 'gap_max': args.gap_max}
-e_id = []
-e_id_bad_eff_dist = ['G347304', 'G351326']
+ev = g.event(args.event).json()
+superevent = ev['superevent']
+pipeline = ev['pipeline']
+mchirp = ev['extra_attributes']['CoincInspiral']['mchirp']
+trig_mc_det = {'central': mchirp, 'delta': mchirp * coeff['m0']}
 
-if args.superevents:
-    e_id = check_superevents(args.superevents)
-if args.events:
-    e_id = check_events(args.events)
+if args.no_redshift:
+    z = {'central': 0.0, 'delta': 0.0}
+    dir_name = 'p_no_redshift'
+else:
+    if pipeline == 'pycbc':
+        ifos = [ifos['ifo'] for ifos in ev['extra_attributes']['SingleInspiral']]
+        eff_dist = [ifo['eff_distance'] if 'eff_distance' in ifo else '-' for
+                    ifo in ev['extra_attributes']['SingleInspiral']]
+        min_eff_distance = min(eff_dist)
+        coinc_snr = ev['extra_attributes']['CoincInspiral']['snr']
+        dist_estimation = coeff['a0'] * min_eff_distance
+        dist_std_estimation = (dist_estimation * math.exp(coeff['b0']) *
+                               coinc_snr ** coeff['b1'])
+        z = z_estimation(dist_estimation, dist_std_estimation)
+        dir_name = 'gracedb_p_dist_estimated'        
+    else:
+        print('Event %s does not have available effective distances.'
+              'Analysis will be performed with no redshift estimation.'
+               % args.event)
+        z = {'central': 0.0, 'delta': 0.0}
+        dir_name = 'gracedb_p_no_redshift'
 
-for event in e_id:
-    ev_data = get_data(event, args)
-    trig_mc = {'central': ev_data['mchirp'], 'delta': ev_data['mchirp_delta']}
-    z = {'central': ev_data['z'], 'delta': ev_data['z_delta']}
-    prob_args = {'trig_mc': trig_mc, 'mass_limits': mass_limits, 'mass_bdary':
-                 mass_bdary, 'z': z, 'mass_gap': args.mass_gap_separate}
-    probabilities = mchirp_area.calc_probabilities(prob_args)
-    name = (ev_data['s_id'] + '/' + str('%s_%s_MG-%s%s.txt' %
-            (ev_data['s_id'], ev_data['e_id'], 'separate' if
-             args.mass_gap_separate is not False else 'together',
-             '_dist-estimated' if args.estimate_lum_dist is True else '')))
-    out_name = 'probabilities/' + name
-    pub_name = '/home/veronica.villa/public_html/mchirp_area/' + name
-    names = [out_name, pub_name]
+areas = mchirp_area.calc_areas(trig_mc_det, mass_limits, mass_bdary, z,
+                               separate_mass_gap)
+total_area = sum(areas.values())
+probabilities = {key: areas[key]/total_area for key in areas}
+
+dirs = [out_dir + dir_name + '/' for out_dir in args.output_dir]
+for dir in dirs:
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+
+name = str('%s-%s-%s.json' % (superevent, args.event, pipeline))
+names = [dir + name for dir in dirs]
+for name in names:
+    with open(name, 'w') as outfile:
+        json.dump(probabilities, outfile)
+
+if args.enable_mass_plot:
+    massplot = mc_area_plots.contour_mass_plot(trig_mc_det, mass_limits,
+                                                   mass_bdary, z,
+                                                   contour_plot_args)
     for name in names:
-        with open(name, 'w') as outfile:
-            outfile.write('Source probabilities for %s \n' % ev_data['s_id'])
-            outfile.write('data: ')
-            json.dump(ev_data, outfile, indent=1)
-            outfile.write('\n' + 'probabilities: ')
-            json.dump(probabilities, outfile, indent=1)
+        plot_name = name.replace('.json', '_mass_plot.png')
+        massplot.savefig(plot_name)
 
-print('Done!')
+if args.enable_probability_plot:
+    probsplot = mc_area_plots.probabilities_plot(probabilities)
+    for name in names:
+        plot_name = name.replace('.json', '_probabilities_pie.png')
+        probsplot.savefig(plot_name)
+
